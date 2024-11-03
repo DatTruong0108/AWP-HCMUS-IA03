4bf3cab42867d00531573b143cac4c7a2c069adb
DatTruong2003 <tandat.truong@mcvnetworks.com>
2024-11-03 12:31:34 +0700

    fix: fix whitelist be

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,12 +1,4 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
-
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -16,6 +8,7 @@ async function bootstrap() {
     origin: `https://awp-hcmus-ia-03.vercel.app`,
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.BE_PORT ?? 3001);
 }
 bootstrap();
