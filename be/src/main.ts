65ca51d3223cee2a1dcbf9a957b1fdcbb1336fbf
DatTruong2003 <tandat.truong@mcvnetworks.com>
2024-11-03 11:58:28 +0700

    update url fe & be

--- be/src/main.ts
+++ be/src/main.ts
@@ -13,7 +13,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `http://localhost:${process.env.FE_PORT ?? 3000}`,
+    origin: `https://awp-hcmus-ia-03.vercel.app/register`,
   });
   await app.listen(process.env.BE_PORT ?? 3001);
 }
