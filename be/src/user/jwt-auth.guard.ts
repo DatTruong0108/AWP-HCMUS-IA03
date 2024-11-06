5d44829a8b99a55a25f02820a7e15c9981efc2d8
DatTruong2003 <tandat.truong@mcvnetworks.com>
2024-11-06 21:54:47 +0700

    finish ia04

--- be/src/user/jwt-auth.guard.ts
+++ be/src/user/jwt-auth.guard.ts
@@ -1,35 +1,3 @@
-// import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
-// import { JwtService } from '@nestjs/jwt';
-// import { Request } from 'express';
-// import { ConfigService } from '@nestjs/config';
-
-// @Injectable()
-// export class JwtAuthGuard implements CanActivate {
-//   constructor(private jwtService: JwtService, private configService: ConfigService) {}
-
-//   canActivate(context: ExecutionContext): boolean {
-//     const request = context.switchToHttp().getRequest<Request>();
-//     const token = this.extractTokenFromHeader(request);
-//     if (!token) {
-//       throw new UnauthorizedException('Token not found');
-//     }
-//     try {
-//       const payload = this.jwtService.verify(token, {
-//         secret: this.configService.get<string>('JWT_SECRET'),
-//       });
-//       request['user'] = payload;
-//     } catch (error) {
-//       throw new UnauthorizedException('Token invalid or expired');
-//     }
-//     return true;
-//   }
-
-//   private extractTokenFromHeader(request: Request): string | undefined {
-//     const [type, token] = (request.headers.authorization || '').split(' ');
-//     return type === 'Bearer' ? token : undefined;
-//   }
-// }
-
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
 import * as jwt from 'jsonwebtoken';
@@ -47,8 +15,8 @@ export class JwtAuthGuard implements CanActivate {
     }
     try {
       // const secret = this.configService.get<string>('JWT_SECRET');
-      // const secret = 'nAgaT0biM4ru@01082003';
-      const secret = process.env.JWT_SECRET;
+      const secret = 'nAgaT0biM4ru@01082003';
+      // const secret = process.env.JWT_SECRET;
       const payload = jwt.verify(token, secret);
       request['user'] = payload;
     } catch (error) {
--- be/src/user/user.service.ts
+++ be/src/user/user.service.ts
@@ -49,8 +49,8 @@ export class UserService {
   
     const payload = { userId: user._id, email: user.email };
     // const secret = this.configService.get<string>('JWT_SECRET');
-    // const secret = 'nAgaT0biM4ru@01082003';
-    const secret = process.env.JWT_SECRET;
+    const secret = 'nAgaT0biM4ru@01082003';
+    // const secret = process.env.JWT_SECRET;
     const accessToken = jwt.sign(payload, secret, { expiresIn: '1h' });
   
     return { accessToken, user: { email: user.email } };
